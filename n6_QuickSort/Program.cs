3955727f1e48836aca4f5cbb594111ba994c778e
Olesya Solovyova <soe611@yandex.ru>
2022-11-09 14:42:50 +0300

    added lesson 6

--- n6_QuickSort/Program.cs
+++ n6_QuickSort/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+          0 1 2  3 4 5 6
+1. arr = [1,0,-6,2,5,3,2]
+2. arr[6] == pivot (опорный элемент)
+3. вызвать шаги 1-2 для подмассива слева от pivot и справа от pivot
+*/
+
+int[] arr = { 0, 5, 2, 3, 5, 9, -1, 7 };
+int[] res = QuickSort(arr, 0, arr.Length - 1);
+Console.WriteLine($"Отсортированный массив: [{string.Join(", ", res)}]");
+
+int[] QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if(minIndex >= maxIndex) return inputArray;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return inputArray;
+}
+int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for (int i = minIndex; i <= maxIndex; i++)
+    {
+        if (inputArray[i] < inputArray[maxIndex])
+        {
+            pivotIndex += 1;
+            //Swap(ref inputArray[pivotIndex], ref inputArray[i]);
+            Swap(inputArray, i, pivotIndex);
+        }
+    }
+    pivotIndex++;
+    Swap(inputArray, pivotIndex, maxIndex);
+    return pivotIndex;
+}
+
+// void Swap(ref int leftValue, ref int rightValue)
+// {
+//     int temp = leftValue;
+//     leftValue = rightValue;
+//     rightValue = temp;
+// }
+
+void Swap(int[] inputArray, int leftValue, int rightValue)
+{
+    int temp = inputArray[leftValue];
+    inputArray[leftValue] = inputArray[rightValue];
+    inputArray[rightValue] = temp;
+}
